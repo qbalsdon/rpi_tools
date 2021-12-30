f8a4bd4fce74c53355c7803fe955d0f14a6d665c
Quintin Balsdon <quintin.balsdon@zuhlke.com>
2021-12-30 16:54:04 +0000

    font style and exit button

--- ui.py
+++ ui.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 import tkinter as tk
 from tkinter import ttk
+from tkinter.font import BOLD, Font
+
+import platform
 
 from common.networking import fetch_ip_address
+
+def Close(main_window):
+    main_window.destroy()
 
 def create_main_window():
     root = tk.Tk()
@@ -10,9 +16,13 @@ def create_main_window():
     root.title("Control UI")
 
     # place a label on the root window
-    message = tk.Label(root, text=f"{fetch_ip_address()}")
+    bold25 = Font(root, size=50, weight=BOLD)
+    message = tk.Label(root, text=f"{fetch_ip_address()} [{platform.platform()}]", font=bold25)
     message.pack()
 
+
+    exit_button = tk.Button(root, text="Exit", command=lambda: Close(root))
+    exit_button.pack(pady=20)
     # keep the window displaying
     root.mainloop()
 
