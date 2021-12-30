67af06ca8922887a36812c65f654f02b24bb5872
Quintin Balsdon <quintin.balsdon@zuhlke.com>
2021-12-30 16:58:51 +0000

    updating networking tools

--- common/networking.py
+++ common/networking.py
@@ -5,5 +5,13 @@ def fetch_ip_address():
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
 
+def get_ip_address():
+    ip_address = '';
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
+
 if __name__ == "__main__":
-    print(f"IP Address [{fetch_ip_address()}]")
+    print(f"IP Address [{fetch_ip_address()}] vs [{get_ip_address()}]")
